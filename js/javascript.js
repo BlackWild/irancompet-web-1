73fe6a0a0e3f12cd2211645ffb6d09468abdbf9e
Siavash <siavash.davani@gmail.com>
2017-09-20 21:13:51 +0430

    add video in modal + smooth scroll

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,15 @@
   $(function () {
 
     $('.parallax').parallax();
+    $("#start").click(function () {
+      $('html, body').animate({
+        scrollTop: $("#toStart").offset().top
+      }, 200);
+    });
+    $('.modal').modal({
+      ready: function (modal, trigger) { $("#vid") },
+      complete: function () { }
+    });
 
   });
 })(jQuery);
